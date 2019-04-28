9771e18eb318896990f0007db2d9f03df30de2bf
James Pearson <james_pearson@outlook.com>
2019-04-28 13:02:15 +0100

    Added cl

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+})
+radio.setGroup(1)
+basic.showIcon(IconNames.Yes)
